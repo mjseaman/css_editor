bf84ef123f18e816f0358faefcabb448ca49f7b4
Mitch Seaman <mitchel.seaman@gmail.com>
2013-07-30 20:50:41 -0700

    completed assignment

--- application.js
+++ application.js
@@ -1,44 +1,18 @@
 $(document).ready(function(){
+	$('#style_editor').submit(function(e){
 
-	$('#new_comment_button').click(function(){
-		showCommentForm();
-	});
-
-	$('#new_comment').submit(function(e){
 		e.preventDefault();
-		addComment();
-	});
+		
+		var selector = $('#selector').val();
+		var property = $('#property').val();
+		var value = $('#value').val();
 
+		updateCSS(selector,property,value);
+
+	});
 });
 
-function Comment(author,text) {
-	this.author = author;
-	this.something = text;
-}
-
-Comment.prototype.render = function() {
-	return ("<li>"+ this.something +"<span class='author'>"+ this.author +"</span></li>");
-}
-
-function showCommentForm() {
-	$('#new_comment_button').hide();
-	$('#new_comment').show();
-}
-
-function hideCommentForm() {
-	$('#new_comment_button').show();
-	$('#new_comment').hide();
-}
-
-function addComment(comment) {
-	var text = $('.comment_text').val();
-	var author = $('.comment_author').val();
-	var comment = new Comment(author,text);
-	$('#comment_list').append(comment.render());
-	hideCommentForm();
-	clearForm($('#new_comment'));
-}
-
-function clearForm(form) {
-	form.children('.field').val("");
+function updateCSS(selector,property,value) {
+	console.log($(selector));
+	$(selector).css(property, value);
 }
